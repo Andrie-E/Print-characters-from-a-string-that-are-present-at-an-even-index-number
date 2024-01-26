bf9006da51db955e5ee0bb0ccda83ac4f2f4d1cf
Andrie-E <stpaul.martinla@gmail.com>
2024-01-26 15:18:49 +0800

    fix the outcome format

--- print_characters_from_a_string.py
+++ print_characters_from_a_string.py
@@ -5,8 +5,9 @@
 Users_input = input("Please write your word here:")
 # Display the users word
 print ("Original String is,", Users_input)
+print("Printing only even index chars")
 # Display characters at even index numbers
 for i in range(0, len(Users_input), 2):
 
 # Print the result
-    print("Printing only even index chars", Users_input[i])
+    print(Users_input[i])
