5f0941c96dc8eb6d928bd74dd8473c7e876a78dc
Andrie-E <stpaul.martinla@gmail.com>
2024-01-26 14:59:33 +0800

    Pseudocode

--- print_characters_from_a_string.py
+++ print_characters_from_a_string.py
@@ -1,1 +1,7 @@
-#Write a program to accept a string from the user and display characters that are present at an even index number.
+# Write a program to accept a string from the user and display characters that are present at an even index number.
+
+# Pseudocode
+# Create an input code for the users to add a word
+# Display the users word
+# Display characters at even index numbers
+# Print the result
