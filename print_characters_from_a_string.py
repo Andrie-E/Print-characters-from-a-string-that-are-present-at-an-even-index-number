804eb3f96c78624ee5c77d47c7e1bacc107a54c7
Andrie-E <stpaul.martinla@gmail.com>
2024-01-26 15:16:38 +0800

    fix the output layout

--- print_characters_from_a_string.py
+++ print_characters_from_a_string.py
@@ -6,9 +6,7 @@ Users_input = input("Please write your word here:")
 # Display the users word
 print ("Original String is,", Users_input)
 # Display characters at even index numbers
-result = ""
 for i in range(0, len(Users_input), 2):
-    result += Users_input[i]
 
 # Print the result
-    print("Printing only even index chars", result)
+    print("Printing only even index chars", Users_input[i])
